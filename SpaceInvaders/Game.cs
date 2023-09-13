8bb239b3c2607349398b3be6f06d4f60f4aa7971
Tianna Paisley <tianna.paisley@mcsystems.com>
2023-09-13 11:56:51 -0500

    #1456 Aliens moving down .

--- SpaceInvaders/Game.cs
+++ SpaceInvaders/Game.cs
@@ -12,6 +12,9 @@ namespace SpaceInvaders
         Player player;
         //Round currentRound;
         bool initializeRound;
+        PictureBox[] aliens;
+        int alienSpeed;
+
 
         //CONSTRUCTORS
         //public Game()
@@ -56,6 +59,7 @@ namespace SpaceInvaders
         {
             player.move();
             this.playerBox.Location = player.Location;
+            MoveAliens(aliens, alienSpeed);
         }
 
 
@@ -63,6 +67,69 @@ namespace SpaceInvaders
         {
             this.player = new Player(100);
             this.player.Location = playerBox.Location;
+        }
+
+        private void Load_aliens()
+        {
+            alienSpeed = 4;
+
+            //allow for the alien images to load from the image folder
+            Image alien1 = Image.FromFile("..\\images\\alien1.png");
+            Image alien2 = Image.FromFile("..\\images\\alien2.png");
+            Image alien3 = Image.FromFile("..\\images\\alien3.png");
+            Image alien4 = Image.FromFile("..\\images\\alien4.png");
+
+            //create arrary for 10 picture boxes 
+            aliens = new PictureBox[14];
+
+            //Initialize the alien picture box
+            for (int i = 0; i < aliens.Length; i++)
+            {
+                aliens[i] = new PictureBox();
+                aliens[i].Size = new Size(40, 40);
+                aliens[i].SizeMode = PictureBoxSizeMode.Zoom;
+                aliens[i].BorderStyle = BorderStyle.None;
+                aliens[i].Visible = false; // ensure that the aliens doesn't show up on the start screen
+                this.Controls.Add(aliens[i]);
+                aliens[i].Location = new Point((i + 1) * 50, -50); //arrange the aliens on the screen
+            }
+
+            aliens[0].Image = alien1;
+            aliens[1].Image = alien2;
+            aliens[2].Image = alien3;
+            aliens[3].Image = alien4;
+            aliens[4].Image = alien2;
+            aliens[5].Image = alien3;
+            aliens[6].Image = alien3;
+            aliens[7].Image = alien2;
+            aliens[8].Image = alien4;
+            aliens[9].Image = alien1;
+            aliens[10].Image = alien4;
+            aliens[11].Image = alien2;
+            aliens[12].Image = alien3;
+            aliens[13].Image = alien1;
+
+        }
+        private void moveAliensTimer1_Tick(object sender, EventArgs e)
+        {
+            MoveAliens(aliens, alienSpeed);
+        }
+
+        // access the alien array and display aliens moving from top to bottom
+        private void MoveAliens(PictureBox[] array, int speed)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i].Visible = true;
+                array[i].Top += speed;
+
+                if (array[i].Top > this.Height)
+                {
+                    array[i].Location = new Point((i + 1) * 50, -200);
+                }
+
+
+            }
         }
 
     }
