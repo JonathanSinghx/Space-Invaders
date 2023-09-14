ec8eef6442aae6349b2b84ffffe6747e703c3531
Abi Matthews-Gordon <Abi.Mgordon@mcsystems.com>
2023-09-14 09:33:13 -0500

    #1448 Player is able to shoot alien.

--- SpaceInvaders/Bullet.cs
+++ SpaceInvaders/Bullet.cs
@@ -10,6 +10,7 @@ namespace SpaceInvaders
     {
         //PROPERTIES
         bool damage;
+        PictureBox sprite;
 
         //CONSTRUCTORS
         public Bullet() : base(new Point(0, 0), 1, 1)
@@ -19,8 +20,20 @@ namespace SpaceInvaders
 
         //GETTERS AND SETTERS
         public bool Damage { get => damage; set => damage = value; }
+        public PictureBox Sprite { get => sprite; set => sprite = value; }
 
 
         //METHODS
+        public void Render(Game game)
+        {
+            sprite = new PictureBox();
+            sprite.Size = new Size(5, 10);
+            sprite.SizeMode = PictureBoxSizeMode.Zoom;
+            sprite.BorderStyle = BorderStyle.None;
+            //sprite.Visible = false;
+            sprite.BackColor = Color.Black;
+            game.Controls.Add(sprite);
+            sprite.Location = new Point(-10, -10);
+        }
     }
 }
